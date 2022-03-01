4f7222b08b2b1cb6b555eddf24c18c4efc12c12d
bolivarc3 <carlsbolivar@gmail.com>
2022-03-01 20:15:59 +0000

    added to the javascript needed for post creation

--- static/scripts/resourcesscript.js
+++ static/scripts/resourcesscript.js
@@ -52,26 +52,21 @@ function createresources(materials){
     if(length > 0){
         //for the amount of images
         for(let i = 0; i < length; i++){
-            // //
-            // const img = [];
-            // img[i] = document.createElement('img');
-            // img[i].src = URL.createObjectURL(event.target.files[i]);
+            material = materials[i];
+            //makes a container for the seperate images
+            tablerow = document.createElement('tr');
+            tablerow.id = "rowbox"
 
-            // //makes a container for the seperate images
-            // const imagediv = [];
-            // imagediv[i] = document.createElement('div');
-            // imagediv[i].id = "imagecontainer" + i;
-            // imagediv[i].classList = "imagecontainer";
+            name = document.createElement('td');
+            name.id = "tablecells"
 
-            // const imagelist = [];
-            // imagelist[i] = document.createElement('li');
-            // imagelist[i].id = "imagelistelement" + i;
-            // //sets the images into the html
-            // document.getElementById("horizontal-list").appendChild(imagelist[i]);
-            // document.getElementById("imagelistelement" + i).appendChild(imagediv[i]);
-            // document.getElementById("imagecontainer" + i).appendChild(img[i]);
-            
-            // create the resources in the table
+            const imagelist = [];
+            imagelist[i] = document.createElement('li');
+            imagelist[i].id = "imagelistelement" + i;
+            //sets the images into the html
+            document.getElementById("horizontal-list").appendChild(imagelist[i]);
+            document.getElementById("imagelistelement" + i).appendChild(imagediv[i]);
+            document.getElementById("imagecontainer" + i).appendChild(img[i]);
         }
     }
 }
