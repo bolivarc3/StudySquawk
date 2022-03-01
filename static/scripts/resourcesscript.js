2d1ace449cac3751743b4d55c6ca57a1e97c5789
bolivarc3 <carlsbolivar@gmail.com>
2022-03-01 20:03:26 +0000

    completed the design of the table resources

--- static/scripts/resourcesscript.js
+++ static/scripts/resourcesscript.js
@@ -24,14 +24,63 @@ async function getresources(course){
     })
     const data = await response.json()
     console.log(data);
+    createresources(data)
 }
 // gets the files from the server to display on the tables
+    // <table id = "resourcestable">
+    //     <tr>
+    //         <th id = "tableheader"><i class="fa-solid fa-circle-check"></i></th>
+    //         <th id = "tableheader" width = "2%"><i class="fa-solid fa-file"></i></th>
+    //         <th id = "tableheader"width ="50%">Name</th>
+    //         <th id = "tableheader", width = "24%">Date Modified</th>
+    //         <th id = "tableheader", width = "24%">Modified By</th>
+    //     </tr>
+    //     <link href="/static/resources/Algebra 1/Capture - Copy.PNG">
+
+    //     <tr id = "rowbox">
+
+    //         <td id = "checkbox"><i class="fa-solid fa-circle-check"></td>
+    //         <td id = "tablecells"><i class="fa-solid fa-file"></i></td>
+    //         <td id = "tablecells"><a href="/static/resources/Algebra 1/Capture - Copy.PNG" target="_blank">Testing File</a></td>
+    //         <td id = "tablecells"><a href="/static/resources/Algebra 1/Capture - Copy.PNG" target="_blank">8/20/2022</a></td>
+    //         <td id = "tablecells"><a href="/static/resources/Algebra 1/Capture - Copy.PNG" target="_blank">codetest</a></td>
+    //     </tr>
+    // </table>
+
+function createresources(materials){
+    var length = materials.length
+    if(length > 0){
+        //for the amount of images
+        for(let i = 0; i < length; i++){
+            // //
+            // const img = [];
+            // img[i] = document.createElement('img');
+            // img[i].src = URL.createObjectURL(event.target.files[i]);
+
+            // //makes a container for the seperate images
+            // const imagediv = [];
+            // imagediv[i] = document.createElement('div');
+            // imagediv[i].id = "imagecontainer" + i;
+            // imagediv[i].classList = "imagecontainer";
+
+            // const imagelist = [];
+            // imagelist[i] = document.createElement('li');
+            // imagelist[i].id = "imagelistelement" + i;
+            // //sets the images into the html
+            // document.getElementById("horizontal-list").appendChild(imagelist[i]);
+            // document.getElementById("imagelistelement" + i).appendChild(imagediv[i]);
+            // document.getElementById("imagecontainer" + i).appendChild(img[i]);
+            
+            // create the resources in the table
+        }
+    }
+}
 
 // Allows for Preview of file
 function showPreview(event){
     var length = event.target.files.length;
     if(length > 0){
-        //for the amount of images 
+        //for the amount of images
         for(let i = 0; i < length; i++){
             //
             const img = [];
