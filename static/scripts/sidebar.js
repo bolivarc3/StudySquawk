1b598edcf709181f38aa96f92f1d5876a9292f5c
bolivarc3 <carlsbolivar@gmail.com>
2023-01-30 13:25:25 -0600

    Initial Creation of Sidebar

--- static/scripts/sidebar.js
+++ static/scripts/sidebar.js
@@ -1,5 +1,7 @@
-const menuIconButton = document.querySelector('[data-menu-icon-btn]')
-
-menuIconButton.addEventListener("click", ()=>{
-    //open sidebar / close sidebar
+const menuIconButton = document.querySelector("[data-menu-icon-btn]")
+console.log(menuIconButton)
+const sidebar = document.querySelector("[data-sidebar]")
+menuIconButton.addEventListener("click", () => {
+  console.log("yo")
+  sidebar.classList.toggle("open")
 })
