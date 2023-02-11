00e4efe4fe0f7a1fe8dc1fd005fabaf114d584ab
bolivarc3 <carlsbolivar@gmail.com>
2023-02-10 21:58:33 -0600

    changed psycop info and added styles

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if ENV == 'dev':
     BUCKET_NAME='studyist-dev'
 else:
     conn = psycopg2.connect(
-        host=os.environ.get('RDS_PORT'),
+        host=os.environ.get('RDS_HOSTNAME'),
         database=os.environ.get('RDS_DB_NAME'),
         user=os.environ.get('RDS_USERNAME'),
         password= os.environ.get('RDS_PASSWORD')
--- static/scripts/sidebar.js
+++ static/scripts/sidebar.js
@@ -1,6 +1,10 @@
 const menuIconButton = document.querySelector("[data-menu-icon-btn]")
-console.log(menuIconButton)
-const sidebar = document.querySelector("[data-sidebar]")
+var sidebar = document.querySelector("[data-sidebar]")
+var header_height = document.querySelector(".header").clientHeight
+console.log(header_height)
+header_height = header_height.toString() + "px"
+console.log()
+document.documentElement.style.setProperty('--header-height', header_height);
 menuIconButton.addEventListener("click", () => {
   console.log("yo")
   sidebar.classList.toggle("open")
