a4a4da4ae0bac1ab8e11d2813ab0c033334628f2
bolivarc3 <carlsbolivar@gmail.com>
2023-08-11 18:52:40 -0500

    change

--- app.py
+++ app.py
@@ -1211,7 +1211,7 @@ def confirm_email(token):
     db_conn.commit()
     db.close()
     db_conn.close()
-    return redirect(url_for('index'))
+    return redirect("/")
 
 def send_mail_confirm(username,email):
     token = s.dumps(email, salt='email-confirm')
