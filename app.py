2c07d07ef10c548ee339d3adf7782ed2d26a91e9
bolivarc3 <carlsbolivar@gmail.com>
2023-08-11 19:06:46 -0500

    change

--- app.py
+++ app.py
@@ -1207,7 +1207,8 @@ def confirm_email(token):
     db_conn = db_info[1]
     password = get_hashed_password(session["attempted_password"])
     print("HELLLOOOW")
-    print(session["user_id_to_confirm"])
+    for i in range(48):
+        print(session["user_id_to_confirm"])
     db.execute('UPDATE "Users" SET is_confirmed=%s WHERE username=%s',("True",session["user_id_to_confirm"],))
     db_conn.commit()
     db.close()
