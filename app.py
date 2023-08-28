6a9fb82e01d777e4347dbd8896cca223d9273639
Carl Bolivar <63116296+bolivarc3@users.noreply.github.com>
2023-08-28 12:02:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ s3 = boto3.client('s3',
     aws_access_key_id = os.environ.get('AWS_S3_ACCESS_KEY'),
     aws_secret_access_key = os.environ.get('AWS_S3_SECRET_ACCESS_KEY'),
         )
-app.secret_key ="secret_key"
+app.secret_key ="super secret key"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = 'filesystem'
