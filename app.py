88764eb4a07c90489f7c0c38f7e6647723c7795e
bolivarc3 <carlsbolivar@gmail.com>
2024-01-03 20:04:10 -0600

    change

--- app.py
+++ app.py
@@ -55,7 +55,6 @@ app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_SSL_VERSION'] = ssl.PROTOCOL_TLSv1_2
 mail = Mail(app)
 
 app.config.from_pyfile('config.cfg')
