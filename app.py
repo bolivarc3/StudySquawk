2f553e38c83230bcc29f6c84a6239219e4981f1c
bolivarc3 <carlsbolivar@gmail.com>
2024-02-14 21:25:55 -0600

    changes to be with selenium

--- app.py
+++ app.py
@@ -30,6 +30,18 @@ import redis
 from datetime import timedelta, time
 from bs4 import BeautifulSoup
 import platform
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver.get("https://medium.com")
+print(driver.page_source)
+print("Finished!")
+
 
 
 
