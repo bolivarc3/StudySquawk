6c2b037185104c9bc243506ddb424ae3923adc59
bolivarc3 <carlsbolivar@gmail.com>
2024-02-16 22:25:16 -0600

    Attempt #2 getting Selenium to work

--- hacapi.py
+++ hacapi.py
@@ -14,6 +14,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 import os
 from app import session
 
