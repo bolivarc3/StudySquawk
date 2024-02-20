ce7ef04a9a4d12d8c6509072ce771ffb71e64646
bolivarc3 <carlsbolivar@gmail.com>
2024-02-19 21:51:09 -0600

    deployed - Fix the subdomain problem

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ if ENV == 'dev':
     BUCKET_NAME='studyist-dev'
     DEBUG_STATUS=True
 else:
-    app.config['SERVER_NAME'] = 'https://www.studysquawk.tech'
+    # app.config['SERVER_NAME'] = 'https://www.studysquawk.tech'
     conn = psycopg2.connect(
         host=os.environ.get('RDS_HOSTNAME'),
         database=os.environ.get('RDS_DB_NAME'),
