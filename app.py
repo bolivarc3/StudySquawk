f4c43c54ade3cf95abdceb802591f557ef469d54
bolivarc3 <carlsbolivar@gmail.com>
2024-02-21 21:28:45 -0600

    change the sever name

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if ENV == 'dev':
     BUCKET_NAME='studyist-dev'
     DEBUG_STATUS=True
 else:
-    app.config['SERVER_NAME'] = 'https://studysquawk.tech'
+    app.config['SERVER_NAME'] = 'studysquawk.tech'
     conn = psycopg2.connect(
         host=os.environ.get('RDS_HOSTNAME'),
         database=os.environ.get('RDS_DB_NAME'),
