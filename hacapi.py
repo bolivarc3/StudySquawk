a266f8897684c116a8b7b0e6c48d32b806a03577
bolivarc3 <carlsbolivar@gmail.com>
2024-04-03 21:18:52 -0500

    fixed it so it actually goes back when doing fetch for hac

--- hacapi.py
+++ hacapi.py
@@ -114,16 +114,15 @@ def hac_api_main(function,api,username,password):
     #returns in a dictionary
 
 def reset(api,driver):
-    if api:
-        try:
-            driver.get("https://hac23.esp.k12.ar.us/HomeAccess/Home/WeekView")
-            wait = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//a[@href="/HomeAccess/Account/Logoff"]')))
-        finally:
-            pass
-        link = driver.find_element(By.XPATH, '//a[@href="/HomeAccess/Account/Logoff"]')
-        link.click()
-        driver.get("https://hac23.esp.k12.ar.us/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f")
+    try:
+        driver.get("https://hac23.esp.k12.ar.us/HomeAccess/Home/WeekView")
+        wait = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//a[@href="/HomeAccess/Account/Logoff"]')))
+    finally:
+        pass
+    link = driver.find_element(By.XPATH, '//a[@href="/HomeAccess/Account/Logoff"]')
+    link.click()
+    driver.get("https://hac23.esp.k12.ar.us/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f")
 
 def grabclasses(driver):
     soup = BeautifulSoup(driver.page_source.encode('utf-8'), features='html.parser')
